e9eaf7f2d219bfe6a0cf4efb2b3a7f5a122ce69d
Emerson Torres <emerson.torres.tec@gmail.com>
2019-12-30 10:00:10 -0300

    Commit inicial

---
+++ src/test/java/teste/marte/sonda/SondaApplicationTests.java
@@ -0,0 +1,13 @@
+package teste.marte.sonda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SondaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
